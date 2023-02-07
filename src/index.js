39e0f7fac882ca4a3f41880d5ea4e2332d8e33ea
Sobyt <kravczov.andrian@mail.ru>
2023-02-07 16:10:11 +0300

    feat: thats was fun

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const strToArray = str.split('') 
+  const openBrackets = bracketsConfig.flat().filter((bracket, i) => i%2 === 0);
+  const closeBrackets =bracketsConfig.flat().filter((bracket, i) => i%2 === 1);
+  const array = []
+  let result = true
+  if(strToArray.length%2 !== 0){
+    result = false
+  }else{
+    strToArray.forEach(element => {
+      if (closeBrackets.includes(element)){
+        if (openBrackets.includes(element)&&!array.includes(element)){
+          array.push(element)
+        }else {
+        let lastElement = array.pop()
+        if(openBrackets.indexOf(lastElement)===closeBrackets.indexOf(element)){
+          result = true
+        }else{
+          result = false
+        }
+      }
+      }else if (openBrackets.includes(element)){
+        array.push(element)
+      }
+    });
+  }
+if (array.length > 0){
+  result = false
+}
+  return result
 }
